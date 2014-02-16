9a4ba142680c6407cceb17c996120872d99bb730
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-02-16 23:43:05 +1100

    Demo problem

--- web/component/my_component.dart
+++ web/component/my_component.dart
@@ -2,7 +2,7 @@ import 'package:angular/angular.dart';
 
 @NgComponent(selector: 'my-component',
     templateUrl: 'component/my_component.html',
-    cssUrl :'component/angular_dart_hello_world_base.css',
+    cssUrls : const ['component/angular_dart_hello_world_base.css'],
     publishAs: 'ctrl')
 class MyComponent {
   showSomething(something, {content}) {
